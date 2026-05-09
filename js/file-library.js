e71ab32d060f3775b5ebe1b3d9af7baed4b1e1b6
indigoxela <user@olga.indigofloat.at>
2026-05-09 11:29:05 +0200

    Issue #7: Fix selecting is broken since core 1.34

--- js/file-library.js
+++ js/file-library.js
@@ -11,7 +11,15 @@
       // active again. Necessary when navigating with views pager.
       let activeValue = $('form.file-managed-file-browser-form [name="fid"]').val();
       if (activeValue) {
-        $('.file-browser-view [data-fid=' + activeValue + ']').closest('.file-library-item').addClass('file-library-selected');
+        if (activeValue.includes(',')) {
+          const fids = activeValue.split(',');
+          for (const fid of fids) {
+            $('.file-browser-view [data-fid=' + fid + ']').closest('.file-library-item').addClass('file-library-selected');
+          }
+        }
+        else {
+          $('.file-browser-view [data-fid=' + activeValue + ']').closest('.file-library-item').addClass('file-library-selected');
+        }
       }
     }
   };
@@ -20,17 +28,44 @@
   Backdrop.fileLibrary.dialogAfterCreate = function(event, dialog, $element) {
     let $browserContainer = $element.find(".file-browser-view");
     if ($browserContainer.length) {
+      let selectedFids = [];
+      const available = Backdrop.settings.file.browser.availableItemCount;
+      const cardinality = Backdrop.settings.file.browser.fieldCardinality;
+      const hint = Backdrop.t('Limit reached, this field can store at most @count items.', { '@count': cardinality });
+      $browserContainer.append('<div id="file-library-limit-hint" popover><div class="icon"></div>' + hint + '</div>');
+      let popover = document.getElementById('file-library-limit-hint');
+      let popoverTimer = 0;
       $browserContainer.once('file-library').on('click keydown', '[data-fid]', function (event) {
         if (event.type === 'keydown' && event.which !== 13) {
           return;
         }
-        $browserContainer.find('.file-library-selected').removeClass('file-library-selected');
-        $(this).closest('.file-library-item').addClass('file-library-selected');
+        let $currentItem = $(this).closest('.file-library-item');
+        if ($currentItem.hasClass('file-library-selected')) {
+          $currentItem.removeClass('file-library-selected');
+          let currentFid = $(this).data('fid');
+          selectedFids = selectedFids.filter(function (fid) { return fid !== currentFid; });
+        }
+        else {
+          if (selectedFids.length >= available) {
+            if (cardinality === 1) {
+              $browserContainer.find('.file-library-selected').removeClass('file-library-selected');
+            }
+            else {
+              window.clearTimeout(popoverTimer);
+              popover.showPopover({ source: this });
+              popoverTimer = window.setTimeout(function() {
+                popover.hidePopover();
+              }, 4000);
+              return;
+            }
+          }
+          $(this).closest('.file-library-item').addClass('file-library-selected');
+          selectedFids.push($(this).data('fid'));
+        }
 
-        let selectedFid = $(this).data('fid');
         // From here on the file module, which provides the "fid" item, takes
         // over.
-        $('form.file-managed-file-browser-form [name="fid"]').val(selectedFid);
+        $('form.file-managed-file-browser-form [name="fid"]').val(selectedFids);
       }).on('dblclick', '[data-fid]', function() {
         // Select this item and submit the form in one go.
         $(this).click();
