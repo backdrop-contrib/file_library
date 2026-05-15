4f9f82f0c4bfd4a323898c0d1e2ed9f6d893f438
indigoxela <xela@indigofloat.at>
2026-05-15 10:58:23 +0200

    Issue #7: Fix selecting is broken since core 1.34, add multiselect (#8)

--- js/file-library.js
+++ js/file-library.js
@@ -11,30 +11,94 @@
       // active again. Necessary when navigating with views pager.
       let activeValue = $('form.file-managed-file-browser-form [name="fid"]').val();
       if (activeValue) {
-        $('.file-browser-view [data-fid=' + activeValue + ']').closest('.file-library-item').addClass('file-library-selected');
+        if (activeValue.includes(',')) {
+          const fids = activeValue.split(',');
+          for (const fid of fids) {
+            $('.file-browser-view [data-fid=' + fid + ']').closest('.file-library-item').addClass('file-library-selected');
+          }
+        }
+        else {
+          $('.file-browser-view [data-fid=' + activeValue + ']').closest('.file-library-item').addClass('file-library-selected');
+        }
       }
     }
   };
 
   Backdrop.fileLibrary = {};
   Backdrop.fileLibrary.dialogAfterCreate = function(event, dialog, $element) {
+    // Do not mess with core's image browser dialog.
+    if ($element.find('.view-image-library').length) {
+      return;
+    }
+    // Prevent Backdrop.file.dialogOpenEvent from breaking things. This trick
+    // works because file-library.js is attached before file.js.
+    // Probably needs tweaks every time something changes core's file.js.
+    let $otherEventContainer = $element.find('.file-browser');
+    // Initialize both offenders in a harmless way.
+    $otherEventContainer.once('file-browser');
+    if (typeof $otherEventContainer.selectable === 'function') {
+      $otherEventContainer.selectable({
+        disabled: true,
+        filter: '.no-such-selector-exists'
+      });
+    }
+
     let $browserContainer = $element.find(".file-browser-view");
     if ($browserContainer.length) {
+      let selectedFids = [];
+      const available = Backdrop.settings.file.browser.availableItemCount;
+      const cardinality = Backdrop.settings.file.browser.fieldCardinality;
+      const hint = Backdrop.t('Limit reached, this field can store at most @count items.', { '@count': cardinality });
+      $browserContainer.append('<div id="file-library-limit-hint" popover><div class="icon"></div>' + hint + '</div>');
+      let popover = document.getElementById('file-library-limit-hint');
+      let popoverTimer = 0;
       $browserContainer.once('file-library').on('click keydown', '[data-fid]', function (event) {
         if (event.type === 'keydown' && event.which !== 13) {
           return;
         }
-        $browserContainer.find('.file-library-selected').removeClass('file-library-selected');
-        $(this).closest('.file-library-item').addClass('file-library-selected');
+        if (event.target.nodeName === 'A') {
+          // Preview link clicked, no bubbling up, let original event fire.
+          event.stopPropagation();
+          return true;
+        }
+        let $currentItem = $(this).closest('.file-library-item');
+        if ($currentItem.hasClass('file-library-selected')) {
+          $currentItem.removeClass('file-library-selected');
+          if (cardinality === 1) {
+            selectedFids = [];
+          }
+          else {
+            let currentFid = $(this).data('fid');
+            selectedFids = selectedFids.filter(function (fid) { return fid !== currentFid; });
+          }
+        }
+        else {
+          if (selectedFids.length >= available) {
+            if (cardinality === 1) {
+              $browserContainer.find('.file-library-selected').removeClass('file-library-selected');
+              selectedFids = [];
+            }
+            else {
+              window.clearTimeout(popoverTimer);
+              popover.showPopover({ source: this });
+              popoverTimer = window.setTimeout(function() {
+                popover.hidePopover();
+              }, 4000);
+              return;
+            }
+          }
+          $(this).closest('.file-library-item').addClass('file-library-selected');
+          selectedFids.push($(this).data('fid'));
+        }
 
-        let selectedFid = $(this).data('fid');
         // From here on the file module, which provides the "fid" item, takes
         // over.
-        $('form.file-managed-file-browser-form [name="fid"]').val(selectedFid);
+        // @see Backdrop.file.dialogCloseEvent
+        $('form.file-managed-file-browser-form [name="fid"]').val(selectedFids);
       }).on('dblclick', '[data-fid]', function() {
-        // Select this item and submit the form in one go.
-        $(this).click();
-        $(this).closest('.ui-dialog').find('.ui-dialog-buttonpane .button-primary').click();
+        // Select this item and submit the form in one go. But only this one item.
+        $('form.file-managed-file-browser-form [name="fid"]').val($(this).data('fid'));
+        $(this).closest('.ui-dialog').find('.ui-dialog-buttonpane .button-primary').trigger('click');
       });
     }
   };
